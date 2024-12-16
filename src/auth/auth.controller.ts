31cfa30bb5b4fb7e504ef74ad7cd9b2fdcdadef0
Wojciech Dlugosz <webmaster@styronet.pl>
2024-12-16 17:13:15 +0100

    Add logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, UseGuards, Request, Response } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request, Response, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
 import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -17,6 +18,15 @@ export class AuthController {
     async login(@Request() req, @Response() res) {
         const tokens = await this.authService.createSession(req.user);
         res.cookie('auth', tokens, { httpOnly: true });
+        res.send({
+            message: 'success',
+        });
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete('logout')
+    async logout(@Response() res) {
+         res.clearCookie('auth', { httpOnly: true });
         res.send({
             message: 'success',
         });
