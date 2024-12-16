c62f0d442f6fbbf468b9da28086de8b84335ba22
Wojciech Dlugosz <webmaster@styronet.pl>
2024-12-16 16:37:58 +0100

    Add registration method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('/register')
+    public register(@Body() userData: RegisterDTO) {
+        return this.authService.register(userData);
+  }
 }
