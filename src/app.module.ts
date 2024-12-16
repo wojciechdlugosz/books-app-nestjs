39ffed9b5a43ad2e9a7bf582c9fc69188e5f29d8
Wojciech Dlugosz <webmaster@styronet.pl>
2024-12-16 14:15:34 +0100

    Add AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,10 @@ import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthorsModule, BooksModule, UsersModule],
+  imports: [AuthorsModule, BooksModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
